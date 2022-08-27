87cf1e390ee003054b9b180bac7bbf95de824b7e
johnlime <thelimejuiceproductions@gmail.com>
2022-08-27 17:46:54 -0400

    prismatic + distance joint test

---
+++ manual_test/prismatic_distance_joint_test_env.py
@@ -0,0 +1,71 @@
+from Box2D import *
+from Box2D.examples.framework import (Framework, Keys, main)
+
+from math import sin, pi, sqrt
+
+class PrismaticDistanceJoint (Framework):
+    name = "PrismaticJoint"
+    description = 'g to stop/go'
+    count = 800
+
+    def __init__(self):
+        Framework.__init__(self)
+
+        body = self.world.CreateStaticBody(
+            position = (10, 0),
+            # (5, 5) in all directions
+            # dimension is (10, 10)
+            shapes = b2PolygonShape(box = (10, 5))
+            )
+
+        limb = self.world.CreateDynamicBody(
+            position = (-5, 0),
+            fixtures = b2FixtureDef(density = 2.0,
+                                    friction = 0.6,
+                                    shape = b2PolygonShape(box = (5, 5)),
+                                    ),
+        )
+
+        self.prismatic_joint = self.world.CreatePrismaticJoint(
+            bodyA = body,
+            bodyB = limb,
+            anchor = (0, 0),
+            axis = (1, 0),
+            lowerTranslation = -5.0,
+            upperTranslation = 0,
+            enableLimit = True,
+            #motorForce = 1.0, #(Doesn't work)
+            motorSpeed = 0.0,
+            enableMotor = True,
+        )
+
+        self.distance_joint = self.world.CreateDistanceJoint(
+            bodyA = body,
+            bodyB = limb,
+            anchorA = (0, 0),
+            anchorB = (0, 0),
+            frequencyHz = 1.0,
+            dampingRatio = 0
+        )
+
+        self.go = False
+        self.time = 0.0
+
+    def Keyboard(self, key):
+        if key == Keys.K_g:
+            self.go = not self.go
+
+    def Step(self, settings):
+        Framework.Step(self, settings)
+
+        if self.go and settings.hz > 0.0:
+            self.time += 1.0 / settings.hz
+
+        renderer = self.renderer
+        renderer.DrawPoint(renderer.to_screen((0, 0)),
+                           4,
+                           b2Color(0.9, 0.9, 0.9)
+                           )
+
+if __name__ == "__main__":
+    main(PrismaticDistanceJoint)
