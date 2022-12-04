dac8164731bbb1bfff79f850b462aa21a088a62f
marszzibros <hwasungishungry@gmail.com>
2022-12-04 13:15:18 -0500

    missing :

--- gym_env/box2d_func.py
+++ gym_env/box2d_func.py
@@ -316,7 +316,7 @@ def raycast(origin_fixture, origin_position, raycast_relative_angle, raycast_dis
     if detected_edge["horizontal"]:
         normal_vector[0], normal_vector[1] = 0, 1
     elif detected_edge["slope"] == 0:
-        if origin_position[0] - origin_fixture.body.position[0]
+        if origin_position[0] - origin_fixture.body.position[0]:
             normal_vector[0], normal_vector[1] = 1, 0
         else:
             normal_vector[0], normal_vector[1] = -1, 0
